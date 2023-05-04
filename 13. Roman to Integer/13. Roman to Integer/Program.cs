8ab7e851357d9b78bcce6043039fe1720332f3c3
ElaJerkovic <92401418+ElaJerkovic@users.noreply.github.com>
2023-05-04 19:34:35 +0200

    C# exercises

---
+++ 13. Roman to Integer/13. Roman to Integer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Roman_to_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string roman;
+            roman = "XIV";
+            Console.WriteLine(RomanToInt(s));
+        }
+        public static int RomanToInt(string s)
+        {
+            int suma = 0;
+            
+
+            for (int i = 0; i < s.Length; i++){
+                if(findValue(s[i - 1]) < findValue(s[i]))
+                {
+                    suma = suma + (findValue(s[i]) - findValue(s[i - 1]));
+                }
+
+                else
+                {
+                    suma = suma + findValue(s[i]);
+                }
+            }
+
+            return suma;
+        }
+        public static int findValue(char chr)
+        {
+            switch (chr)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return 0;
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            Console.WriteLine(sol.IsValid("{[]([])}"));
+            Console.ReadLine();
+        }
+
+    }
+
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            int n = 0;
+            while(s.Length != n)
+            {
+                n = s.Length;
+                s = s.Replace("()", "");
+                s = s.Replace("[]", "");
+                s = s.Replace("{}", "");
+            }
+
+            if (s.Length == 0) return true;
+            else return false;
+        }
+    }
+}
